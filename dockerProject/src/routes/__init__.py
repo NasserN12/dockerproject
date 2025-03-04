db4884f7a50c0316c6cb95d447bf22f498b1c5fb
Nassernas <166876600+Nassernas@users.noreply.github.com>
2025-03-04 14:39:45 -0700

    docker

---
+++ dockerProject/src/routes/__init__.py
@@ -0,0 +1,86 @@
+def init_routes(app):
+    from flask import request, jsonify
+    from database import get_db_connection
+    import mysql.connector
+
+    @app.route('/student', methods=['POST'])
+    def student():
+        data = request.json
+        connection = None
+        cursor = None
+
+        try:
+            connection = get_db_connection()
+            cursor = connection.cursor()
+
+            cursor.execute('SELECT * FROM student WHERE studentID = %s', (data['studentID'],))
+            existing_student = cursor.fetchone()
+
+            if existing_student:
+                return jsonify({"message": "Student already exists"}), 409
+
+
+            cursor.execute('''
+                INSERT INTO student (studentID, studentName, course, presentDate)
+                VALUES (%s, %s, %s, %s)
+            ''', (data['studentID'], data['studentName'], data['course'], data['presentDate']))
+            connection.commit()
+
+            return jsonify({"message": "Student Created Successfully"}), 201
+
+        except mysql.connector.Error as e:
+            return jsonify({"error": str(e)}), 500
+
+        except KeyError:
+            return jsonify({"error": "Invalid data format"}), 400
+
+        finally:
+            if cursor:
+                cursor.close()
+            if connection:
+                connection.close()
+
+    @app.route('/student/<string:studentID>', methods=['PUT'])
+    def update_student(studentID):
+        data = request.json
+        connection = None
+        cursor = None
+
+        try:
+            connection = get_db_connection()
+            cursor = connection.cursor()
+
+            # Check if student exists
+            cursor.execute('SELECT * FROM student WHERE studentID = %s', (studentID,))
+            existing_student = cursor.fetchone()
+
+            if not existing_student:
+                return jsonify({"message": "Student not found"}), 404
+
+            # Update student record
+            cursor.execute('''
+                UPDATE student 
+                SET studentName = %s, course = %s, presentDate = %s
+                WHERE studentID = %s
+            ''', (data['studentName'], data['course'], data['presentDate'], studentID))
+            connection.commit()
+
+            return jsonify({"message": "Student Updated Successfully"}), 200
+
+        except mysql.connector.Error as e:
+            return jsonify({"error": str(e)}), 500
+
+        except KeyError:
+            return jsonify({"error": "Invalid data format"}), 400
+
+        finally:
+            if cursor:
+                cursor.close()
+            if connection:
+                connection.close()
+
+
+    
+
+
+
